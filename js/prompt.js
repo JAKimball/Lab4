9af9806f7c0b4d63d1c06d9b5761a497951f545e
Jonathan A. Kimball <jonathan.kimball@comcast.net>
2019-05-23 16:46:39 -0700

    1st functions draft.  Columns fit but stying could be better.

--- js/prompt.js
+++ js/prompt.js
@@ -2,5 +2,21 @@ var userColumnResponse = prompt("How many articles on each page?");
 var numColumns = Number(userColumnResponse); 
 
 function articleWidth() {
-    return 1 / numColumns;
-}
+    return 100 / numColumns;
+};
+
+function article() {
+    var output = '<article style="width: ' + articleWidth() + '%;">';
+    output += '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas quisquam doloremque, quas sequi laudantium voluptate. Veniam quia veritatis eius animi natus dolorem nulla accusantium maiores at praesentium! Cupiditate, enim?</p>';
+    output += '<p>Assumenda quibusdam vero placeat esse eius odit hic tempora adipisci eum officia dolores maxime a voluptates, quisquam, vitae perferendis unde, eligendi molestiae labore quos velit soluta dolor iure. Inventore, quaerat?</p>';
+    output += '</article>';
+    return output;
+};
+
+function articles() {
+    var output = '';
+    for (let i = 0; i < numColumns * 2; i++) {
+        output += article();
+    };
+    return output;
+};

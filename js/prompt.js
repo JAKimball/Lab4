61169ccc416ac77dff95b440723d3b534100f4f2
Jonathan A. Kimball <jonathan.kimball@comcast.net>
2019-06-03 23:06:25 -0700

    eslint fixes to JavaScript

--- js/prompt.js
+++ js/prompt.js
@@ -7,67 +7,67 @@ function articleWidth() {
 
 function article1() {
   var output = '<article style="width: ' + articleWidth() + '%;">';
-  output += "<section>";
+  output += '<section>';
   output +=
-    "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas quisquam doloremque, quas sequi laudantium voluptate. Veniam quia veritatis eius animi natus dolorem nulla accusantium maiores at praesentium! Cupiditate, enim?</p>";
+    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas quisquam doloremque, quas sequi laudantium voluptate. Veniam quia veritatis eius animi natus dolorem nulla accusantium maiores at praesentium! Cupiditate, enim?</p>';
   output +=
-    "<p>Assumenda quibusdam vero placeat esse eius odit hic tempora adipisci eum officia dolores maxime a voluptates, quisquam, vitae perferendis unde, eligendi molestiae labore quos velit soluta dolor iure. Inventore, quaerat?</p>";
-  output += "</section>";
-  output += "</article>";
+    '<p>Assumenda quibusdam vero placeat esse eius odit hic tempora adipisci eum officia dolores maxime a voluptates, quisquam, vitae perferendis unde, eligendi molestiae labore quos velit soluta dolor iure. Inventore, quaerat?</p>';
+  output += '</section>';
+  output += '</article>';
   return output;
 }
 
 function article2() {
   var output = '<article style="width: ' + articleWidth() + '%;">';
-  output += "<section>";
+  output += '<section>';
   output +=
-    "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas quisquam doloremque, quas sequi laudantium voluptate. Veniam quia veritatis eius animi natus dolorem nulla accusantium maiores at praesentium! Cupiditate, enim?</p>";
+    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas quisquam doloremque, quas sequi laudantium voluptate. Veniam quia veritatis eius animi natus dolorem nulla accusantium maiores at praesentium! Cupiditate, enim?</p>';
   output +=
-    "<p>Nulla, quaerat culpa odit illo, dolore harum doloribus corporis obcaecati iste assumenda tempora veritatis nam nesciunt quibusdam, dolorem enim eum cupiditate deserunt! Animi impedit suscipit unde iusto debitis eveniet nulla!</p>";
+    '<p>Nulla, quaerat culpa odit illo, dolore harum doloribus corporis obcaecati iste assumenda tempora veritatis nam nesciunt quibusdam, dolorem enim eum cupiditate deserunt! Animi impedit suscipit unde iusto debitis eveniet nulla!</p>';
   output +=
-    "<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero possimus, voluptates debitis atque qui accusamus labore doloribus consequuntur! At quasi doloribus itaque voluptas nesciunt excepturi possimus minus fugiat quo ea!</p>";
-  output += "</section>";
-  output += "</article>";
+    '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero possimus, voluptates debitis atque qui accusamus labore doloribus consequuntur! At quasi doloribus itaque voluptas nesciunt excepturi possimus minus fugiat quo ea!</p>';
+  output += '</section>';
+  output += '</article>';
   return output;
 }
 
 function article3() {
   var output = '<article style="width: ' + articleWidth() + '%;">';
-  output += "<section>";
+  output += '<section>';
   output +=
-    "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas quisquam doloremque, quas sequi laudantium voluptate. Veniam quia veritatis eius animi natus dolorem nulla accusantium maiores at praesentium! Cupiditate, enim?</p>";
+    '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas quisquam doloremque, quas sequi laudantium voluptate. Veniam quia veritatis eius animi natus dolorem nulla accusantium maiores at praesentium! Cupiditate, enim?</p>';
   output +=
-    "<p>Et netus et malesuada fames. Mattis rhoncus urna neque viverra justo nec ultrices. Amet massa vitae tortor condimentum lacinia quis vel. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Eu scelerisque felis imperdiet proin fermentum leo vel. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo.</p>";
+    '<p>Et netus et malesuada fames. Mattis rhoncus urna neque viverra justo nec ultrices. Amet massa vitae tortor condimentum lacinia quis vel. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Eu scelerisque felis imperdiet proin fermentum leo vel. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo.</p>';
   output += '<img src="images/logo.jpg" alt="logo">';
   output +=
-    "<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero possimus, voluptates debitis atque qui accusamus labore doloribus consequuntur! At quasi doloribus itaque voluptas nesciunt excepturi possimus minus fugiat quo ea!</p>";
-  output += "</section>";
-  output += "</article>";
+    '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero possimus, voluptates debitis atque qui accusamus labore doloribus consequuntur! At quasi doloribus itaque voluptas nesciunt excepturi possimus minus fugiat quo ea!</p>';
+  output += '</section>';
+  output += '</article>';
   return output;
 }
 
 function promptForColumns() {
-  var userResponse = "";
+  var userResponse = '';
 
   // Keep prompting until the user gives a number from 1 to 10.
   do {
     userResponse = prompt(
-      "How many columns (1 thru 10) should we display articles across each page?"
+      'How many columns (1 thru 10) should we display articles across each page?'
     );
   } while (isNaN(userResponse) || userResponse < 1 || userResponse > 10);
   return userResponse;
 }
 
 function articles() {
-  var output = "";
-  var articlePick = 0;
+  var output = '';
+  // var articlePick;
 
   for (let i = 0; i < numColumns * 3; i++) {
     let articlePick = Math.floor(Math.random() * 3);
 
-    if (articlePick == 0) {
+    if (articlePick === 0) {
       output += article1();
-    } else if (articlePick == 1) {
+    } else if (articlePick === 1) {
       output += article2();
     } else {
       output += article3();
